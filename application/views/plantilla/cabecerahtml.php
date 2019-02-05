69612b707bd03a7c78a44adbb42203766b81a4a7
ricardguachalla <47367224+ricardguachalla@users.noreply.github.com>
2019-02-05 16:02:30 -0400

    Revert "cambie mi titulo de mi pagina"
    This reverts commit 61e06a68ae5dcca77540478f0025f147b55c233c.

--- application/views/plantilla/cabecerahtml.php
+++ application/views/plantilla/cabecerahtml.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>sistema sistematienda</title>
+    <title>sistema</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="<?=base_url();?>bootstrap/css/bootstrap.min.css" rel="stylesheet">
